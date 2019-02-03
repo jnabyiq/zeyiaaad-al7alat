b8c98626fd51c2c124d7a34f1ed49c818a34a6f7
JnabY <43906195+jnabyiq@users.noreply.github.com>
2019-02-04 00:49:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,9 +16,9 @@ bot.on("ready", () => {
 
 });
 
-const ownerid = "412253155473096715";
+const ownerid = "462660314765328394";
 
-const prefix = "N";
+const prefix = "-";
 
 bot.on('message', message => {
 
